0adf3f6f49f201e8838ae5a280ac99d3bfb458c2
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-12-29 14:28:40 +0600

    added restaurant intro section in the homepage

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
 import Header from '../../../Shared/Header/Header';
+import RestaurantIntro from '../RestaurantIntro/RestaurantIntro';
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <div className='my-12 md:my-16'>
                 <Header title={'From 11:00am to 10:00pm'} subTitle={'Order Online'}></Header>
                 <Category></Category>
+                <RestaurantIntro></RestaurantIntro>
             </div>
         </div>
     );
---
+++ src/Pages/Home/RestaurantIntro/RestaurantIntro.jsx
@@ -0,0 +1,23 @@
+import chef from '../../../assets/home/chef-service.jpg';
+
+const RestaurantIntro = () => {
+    return (
+        <div className='my-12 md:my-16 w-[90%] mx-auto max-w-7xl flex justify-center items-center'
+            style={{
+                backgroundImage: `url(${chef})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '572px', 
+                width: '100%'
+            }}
+        >
+            <div className='bg-white w-[90%] py-16 md:w-[80%] text-center'>
+                <h1 className='font-cinzel text-[#151515] text-[45px] '>Bistro Boss</h1>
+                <p className='text-[#151515] w-[95%] md:w-[80%] mx-auto'>Welcome to Bistro Boss – where culinary artistry meets unforgettable dining experiences. Savor world-class cuisines crafted with passion, indulge in impeccable service, and immerse yourself in a warm, inviting ambiance that feels like home away from home.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default RestaurantIntro;
